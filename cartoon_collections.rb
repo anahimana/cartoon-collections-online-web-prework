27fe116e26a8662b335942d14968d58243e167ca
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 16:16:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,5 @@ def find_the_cheese# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
 
-my_arr = ['er', 'rr', 'eee']
-puts long_planeteer_calls(my_arr)
+# my_arr = ['er', 'rr', 'eee']
+# puts long_planeteer_calls(my_arr)
