dae8f4c418093ef15d8f7015d8c9fd26359b6425
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 16:31:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ end
 def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
-    return arr.include?("cheese")
+    arr.include?("cheese")
   end
 end
 
