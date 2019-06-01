f38b626db70e365d50e67169cb2b69504ef42f4b
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 15:39:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(arr)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arr)# code an argument here
   # Your code here
 end
 
