c2bf742e73c05c32ba7b98fefbf161e6b2f7e782
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 16:12:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(arr)
 end
 
 def summon_captain_planet(arr)
-  arr.collect { |i| i.capitalize }
+  arr.collect { |i| i.capitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here
